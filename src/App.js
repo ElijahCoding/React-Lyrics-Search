39bc59beac887e4932f1c476cf6d95fe5c28e7f3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-11 23:23:53 +0300

    files structure built

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Index from './components/layout/Index'
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Navbar />
-        <h1>Hi</h1>
-      </div>
+        <Router>
+            <React.Fragment>
+                <Navbar />
+                <div className="container">
+                    <Switch>
+                        <Route exac path="/" component={ Index } />
+                    </Switch>
+                </div>
+            </React.Fragment>
+        </Router>
     );
   }
 }
