26019892fee7244b7d265f6651aceffe083990e9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-12 09:22:37 +0300

    lyrics route built

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Index from './components/layout/Index'
+import Lyrics from './components/tracks/Lyrics'
 
 import { Provider } from './context'
 
@@ -16,7 +17,8 @@ class App extends Component {
                     <Navbar />
                     <div className="container">
                         <Switch>
-                            <Route exac path="/" component={ Index } />
+                            <Route exact path="/" component={ Index } />
+                            <Route exact path="/lyrics/track/:id" component={Lyrics} />
                         </Switch>
                     </div>
                 </React.Fragment>
