0294a6be04839967831cd00101562c080eca016d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-12 08:12:20 +0300

    fetching lyrics

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -6,7 +6,6 @@ class Tracks extends Component {
         return (
             <Consumer>
                 { value => {
-                    console.log(value);
                     return <h1>Tracks</h1>
                 } }
             </Consumer>
